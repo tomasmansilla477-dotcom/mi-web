5ceb3e95c5b3bd6edc3779a8889ccdfcd84d1d8b
tomasmansilla477-dotcom <tomasmansilla477@gmail.com>
2026-06-13 23:39:55 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11143451
+    "domain": "3nbf4.com",
+    "zoneId": 11145319
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
